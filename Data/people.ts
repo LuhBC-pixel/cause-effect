5fabc3db78753f2beedef82e52a7b4c2670c3721
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2022-01-11 17:37:13 -0300

    create data

---
+++ Data/people.ts
@@ -0,0 +1,74 @@
+interface Person {
+  id: number;
+  name: string;
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  telephone: string;
+  birthday: string;
+  image: string;
+}
+
+export const people: Person[] = [
+  {
+    id: 1,
+    name: 'Roseanne Rezende Salles',
+    street: 'Rua 13 de Julho',
+    city: 'Várzea Grande',
+    state: 'MT',
+    country: 'BR',
+    telephone: '(65) 2885-7359',
+    birthday: '26/12/1991',
+    image:
+      'https://image.freepik.com/free-photo/portrait-beautiful-young-woman-standing-grey-wall_231208-10760.jpg',
+  },
+  {
+    id: 2,
+    name: 'Geneval Fernandes Amancio',
+    street: 'Rua Projetada 305',
+    city: 'Picos',
+    state: 'PI',
+    country: 'BR',
+    telephone: '(89) 3890-5432',
+    birthday: '08/09/1992',
+    image:
+      'https://image.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg',
+  },
+  {
+    id: 3,
+    name: 'Bianca Veiga Chiles',
+    street: 'Rua Professora Maria Sales',
+    city: 'João Pessoa',
+    state: 'PB',
+    country: 'BR',
+    telephone: '(83) 3432-6241',
+    birthday: '10/04/1996',
+    image:
+      'https://image.freepik.com/free-photo/charismatic-cheerful-attractive-african-american-woman-curly-haircut-wearing-shirt-holding-hands-pockets-confident-outgoing-smiling-talking-pleasant-conversation-feeling-self-assured-relaxed_176420-35345.jpg',
+  },
+  {
+    id: 4,
+    name: 'Mario Diniz de Padua',
+    street: 'Rua Doutor Euler de Azevedo',
+    city: 'Campo Grande',
+    state: 'MS',
+    country: 'BR',
+    telephone: '(67) 3821-2803',
+    birthday: '26/03/1979',
+    image:
+      'https://image.freepik.com/free-photo/handsome-young-african-american-with-khaki-tshirt_176420-32047.jpg',
+  },
+  {
+    id: 5,
+    name: 'Silezia Marica Lopes',
+    street: 'Rua Expedicionário Pontes Lira',
+    city: 'Parnamirim',
+    state: 'RN',
+    country: 'BR',
+    telephone: '(84) 3966-8454',
+    birthday: '20/03/1973',
+    image:
+      'https://image.freepik.com/free-photo/friendly-smiling-happy-asian-girl-give-advice-where-find-what-need-pointing-fingers-left-grinning-carefree-recommend-advertisement-promote-product-discuss-new-store-white-wall_176420-37167.jpg',
+  },
+];
